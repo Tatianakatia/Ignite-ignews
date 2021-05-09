18087e460b76ad0153eed9b0d698243d6e657bf0
tatianakatia <tatiana.katia@gmail.com>
2021-05-09 15:45:06 +0100

    start config prismic

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -7,7 +7,6 @@ export default class MyDocument  extends Document {
         <Head>
           <link rel="preconnect" href="https://fonts.gstatic.com" />
           <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700;900&display=swap" rel="stylesheet" />
-          <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600;700&display=swap" rel="stylesheet" />
 
           <link rel="shortcut icon" href="/favicon.png" type="image/png"/>
         </Head>
@@ -16,6 +15,6 @@ export default class MyDocument  extends Document {
           <NextScript />
         </body>
       </Html>
-    )
+    );
   }
 }
--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -8,26 +8,26 @@ export default function Post() {
         <title>Posts | Ignews </title>
       </Head>
 
-      <main>
-        <div>
-          <a>
+      <main className={styles.container}>
+        <div className={styles.posts}>
+          <a href="#">
             <time>12 of March of 2021</time>
             <strong>How do you make a good README</strong>
             <p>Guia Rocketseat para um README de qualidade. Se você não leu o post O que é README recomendo que leia, se já sabe da importância desse arquivo no projeto, pode continuar por aqui.</p>
           </a>
-          <a>
-            <time>12 of March of 2021</time>
-            <strong>How do you make a good README</strong>
-            <p>Guia Rocketseat para um README de qualidade. Se você não leu o post O que é README recomendo que leia, se já sabe da importância desse arquivo no projeto, pode continuar por aqui.</p>
-          </a>
-
-          <a>
+          <a href="#">
             <time>12 of March of 2021</time>
             <strong>How do you make a good README</strong>
             <p>Guia Rocketseat para um README de qualidade. Se você não leu o post O que é README recomendo que leia, se já sabe da importância desse arquivo no projeto, pode continuar por aqui.</p>
           </a>
 
-          <a>
+          <a href="#">
+            <time>12 of March of 2021</time>
+            <strong>How do you make a good README</strong>
+            <p>Guia Rocketseat para um README de qualidade. Se você não leu o post O que é README recomendo que leia, se já sabe da importância desse arquivo no projeto, pode continuar por aqui.</p>
+          </a>
+
+          <a href="#">
             <time>12 of March of 2021</time>
             <strong>How do you make a good README</strong>
             <p>Guia Rocketseat para um README de qualidade. Se você não leu o post O que é README recomendo que leia, se já sabe da importância desse arquivo no projeto, pode continuar por aqui.</p>
